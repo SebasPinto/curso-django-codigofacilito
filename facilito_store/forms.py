448c2e6b10974804147407ce9fb6faada49dbcad
Sebas Pinto <85.jspo@gmail.com>
2019-09-06 16:50:33 -0500

    First commit

---
+++ facilito_store/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(required=True,
+                                min_length=4,
+                                max_length=50,
+                                widget=forms.TextInput(attrs={
+                                    'class': 'form-control mb-4',
+                                    'placeholder': 'User Name'
+                                }))
+    email = forms.EmailField(required=True,
+                            widget=forms.EmailInput(attrs={
+                                    'class': 'form-control mb-4',
+                                    'placeholder': 'Email'
+                                }))
+    password=forms.CharField(required = True,
+                                min_length = 8,
+                                max_length = 12,
+                                widget=forms.PasswordInput(attrs={
+                                    'class': 'form-control mb-4',
+                                    'placeholder': 'Password'
+                                }))
+
+    password2=forms.CharField(required = True,
+                                min_length = 8,
+                                max_length = 12,
+                                widget=forms.PasswordInput(attrs={
+                                    'class': 'form-control',
+                                    'placeholder': 'Repeat Password'
+                                }))
+
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('User Name already exists')
+
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Email already exists')
+
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if cleaned_data.get('password2') != cleaned_data.get('password'):
+            self.add_error('password2', 'Passwords not matching')
+
+
---
+++ facilito_store/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, redirect
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+from .forms import RegisterForm
+
+def index(request):
+    return render(request, 'index.html', {
+        #context
+    })
+
+
+def login_view(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login(request, user)
+            messages.success(request, f'Welcome {user.username}', extra_tags='success')
+            return redirect('index')
+        else:
+            messages.error(request, f'Username or Password is incorrect', extra_tags='danger')
+    return render(request, 'users/login.html',{
+
+    })
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Logout Successfuly', extra_tags='info')
+    return redirect('login')
+
+
+def register(request):
+    form = RegisterForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+
+        new_user = User.objects.create_user(username, email, password)
+
+        if new_user:
+            login(request, new_user)
+            messages.success(request, 'User Created', extra_tags='success')
+            return redirect('index')
+
+    return render(request, 'users/register.html',{
+        'form':form,
+
+    })
+
+ 
